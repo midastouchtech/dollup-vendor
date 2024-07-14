9904c96a7b710b3f1b0ad3d0262c82ebc90d5a3a
Ayabonga Qwabi <aya@simply.co.za>
2024-07-14 17:43:09 +0200

    add more seo

--- components/layouts/modules/Head.jsx
+++ components/layouts/modules/Head.jsx
@@ -5,17 +5,116 @@ const StyleSheets = () => (
   <Head>
     <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
     <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-    <meta name='format-detection' content='telephone=no' />
+    <meta name='format-detection' content='telephone=yes' />
     <meta name='apple-mobile-web-app-capable' content='yes' />
     {/*<link rel="shortcut icon" href="/static/img/favi.png" />
         <link rel="icon" href="/static/img/favi.png" sizes="32x32" />
         <link rel="icon" href="/static/img/favi.png" sizes="192x192" />
         <link rel="apple-touch-icon-precomposed" href="/static/img/favi.png" />*/}
+    <title>
+      Dollup for Business - Sign Up Your Salon and Serve Services Online
+    </title>
+
+    <meta
+      name='description'
+      content='Join Dollup for Business and sign up your salon to offer beauty services online. Reach more customers and manage appointments effortlessly. Create an account today and grow your salon with Dollup.'
+    />
+    <meta
+      name='keywords'
+      content='Dollup for Business, salon sign up, beauty services online, manage appointments, salon account creation, vendor platform, grow your salon, beauty salon management, online salon services, salon vendors'
+    />
+    <meta name='author' content='Dollup for Business' />
+    <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+
     <meta name='author' content='nouthemes' />
     <meta name='keywords' content='Dollup, react, ecommerce, Template' />
     <meta name='description' content='Dollup - React eCommerce Template' />
+    <meta
+      property='og:title'
+      content='Dollup for Business - Sign Up Your Salon and Serve Services Online'
+    />
+    <meta
+      property='og:description'
+      content='Join Dollup for Business and sign up your salon to offer beauty services online. Reach more customers and manage appointments effortlessly. Create an account today and grow your salon with Dollup.'
+    />
+    <meta
+      property='og:image'
+      content='https://www.vendors.touch.net.za/og.jpg'
+    />
+    <meta property='og:url' content='https://www.vendors.touch.net.za' />
+    <meta property='og:type' content='website' />
+    <meta property='og:site_name' content='Dollup for Business' />
+
+    <meta name='twitter:card' content='summary_large_image' />
+    <meta
+      name='twitter:title'
+      content='Dollup for Business - Sign Up Your Salon and Serve Services Online'
+    />
+    <meta
+      name='twitter:description'
+      content='Join Dollup for Business and sign up your salon to offer beauty services online. Reach more customers and manage appointments effortlessly. Create an account today and grow your salon with Dollup.'
+    />
+    <meta
+      name='twitter:image'
+      content='https://www.vendors.touch.net.za/og.jpg'
+    />
+    <meta name='twitter:site' content='@DollupBusiness' />
+    <meta name='twitter:creator' content='@DollupBusiness' />
+    <meta name='twitter:domain' content='https://www.vendors.touch.net.za' />
+
+    <link rel='icon' href='/favicon/favicon.ico' type='image/x-icon' />
+    <link
+      rel='apple-touch-icon'
+      sizes='180x180'
+      href='/favicon/apple-touch-icon.png'
+    />
+    <link
+      rel='icon'
+      type='image/png'
+      sizes='32x32'
+      href='/favicon/favicon-32x32.png'
+    />
+    <link
+      rel='icon'
+      type='image/png'
+      sizes='16x16'
+      href='/favicon/favicon-16x16.png'
+    />
+    <link rel='manifest' href='/favicon/site.webmanifest' />
+    <link
+      rel='mask-icon'
+      href='/favicon/safari-pinned-tab.svg'
+      color='#5bbad5'
+    />
+    <meta name='msapplication-TileColor' content='#06b6d4' />
+    <meta name='theme-color' content='#06b6d4' />
+
+    <meta name='apple-mobile-web-app-capable' content='yes' />
+    <meta
+      name='apple-mobile-web-app-status-bar-style'
+      content='black-translucent'
+    />
+    <meta name='apple-mobile-web-app-title' content='Dollup for Business' />
+
+    <meta name='mobile-web-app-capable' content='yes' />
+    <meta name='application-name' content='Dollup for Business' />
+
+    <meta name='robots' content='index, follow' />
+    <meta name='googlebot' content='index, follow' />
+    <meta name='bingbot' content='index, follow' />
+    <meta name='rating' content='general' />
+    <meta name='distribution' content='global' />
+    <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+    <meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
+    <meta http-equiv='Content-Language' content='en' />
+    <meta http-equiv='Content-Script-Type' content='text/javascript' />
+    <meta http-equiv='Content-Style-Type' content='text/css' />
+    <meta
+      http-equiv='Content-Location'
+      content='https://www.dollup.touch.net.za'
+    />
+
     <script src='https://maps.googleapis.com/maps/api/js?key=AIzaSyDyQM3_-efaShAM-axXn1jKyTO5rOJKkdQ&libraries=places'></script>
-    <title>Dollup</title>
     <link
       href='https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,600,700&amp;amp;subset=latin-ext'
       rel='stylesheet'
